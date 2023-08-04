2f7a0ad44a90be17ed1e53a8359159493c22ae70
Zakhar Griaznov <zkhrg@zkhrg-macbook-air.local>
2023-08-04 03:09:15 +0300

    made todo: init logger slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,6 +3,15 @@ package main
 import (
 	"fmt"
 	"go-url-shortener/internal/config"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -13,10 +22,34 @@ func main() {
 
 	// TODO: init logger: slog
 
+	log := setLogger(cfg.Env)
+
+	log.Info("starting go-url-shortener", slog.String("env", cfg.Env))
+
 	// TODO: init storage: sqlite3
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
 
+func setLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
